50981ed42d3bc4211c90b46180f522610d904a6d
jestupinanb <30636279+jestupinanb@users.noreply.github.com>
2020-11-30 16:43:50 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        try{
+            // crear un analizador léxico que se alimenta a partir de la entrada (archivo  o consola)
+            Python3Lexer lexer;
+            if (args.length>0)
+                lexer = new Python3Lexer(CharStreams.fromFileName(args[0]));
+            else
+                lexer = new Python3Lexer(CharStreams.fromStream(System.in));
+            // Identificar al analizador léxico como fuente de tokens para el sintactico
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            // Crear el objeto correspondiente al analizador sintáctico que se alimenta a partir del buffer de tokens
+            Python3Parser parser = new Python3Parser(tokens);
+            ParseTree tree = parser.single_input(); // Iniciar el analisis sintáctico en la regla inicial: r
+
+        } catch (Exception e){
+            System.err.println("Error (Test): " + e);
+        }
+    }
+}

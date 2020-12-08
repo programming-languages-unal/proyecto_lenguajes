b9cc337c5a606d3020f8e01b00f85a91e0299c08
unknown <afsanchezsa@unal.edu.co>
2020-12-07 19:38:19 -0500

    added some detail in test input

--- input/in.java
+++ input/in.java
@@ -169,8 +169,12 @@ class TestFourthRule{
 * 4.8.2.1 One variable per declaration
 * */
 class LastRule{
+    int s;//no da error
+    int k;//no da error
     int a,b;//error 4.8.2.1
     void me(){
+        int c;//no da error
+        int s;//no da error
         int a,b;//error 4.8.2.1
     }
 

b5079013d19623017a7911a3990278369828434f
juan <jpbmdev@gmail.com>
2020-12-11 21:27:07 -0500

    3.4.1

--- input/in.java
+++ input/in.java
@@ -1,3 +1,7 @@
 class outerClass {
 
 }
+
+class outerClass2 {
+
+}
--- src/SecondVisitor.java
+++ src/SecondVisitor.java
@@ -1,19 +1,25 @@
 public class SecondVisitor <T> extends Java9BaseVisitor {
 
+    Boolean isclass = false;
+
+
     void error(String error) {
-        try {
-            System.out.println(error);
-            throw new IllegalAccessException("Error!");
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
+        //try {
+        System.out.println(error);
+        //throw new IllegalAccessException("Error!");
+        //} catch (IllegalAccessException e) {
+        //    e.printStackTrace();
+        //}
     }
 
     @Override
     public Object visitNormalClassDeclaration(Java9Parser.NormalClassDeclarationContext ctx) {
         if (ctx.identifier() != null) {
-            String identifierText = ctx.identifier().getText();
-            System.out.println(identifierText);
+            if(isclass == false){
+                isclass = true;
+            }else{
+                error("error: violacion de la regla 3.4.1 Cada clase de nivel superior reside en un archivo fuente propio, linea: "+ ctx.CLASS().getSymbol().getLine());
+            }
 
         }
         return null;

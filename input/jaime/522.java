49bab46be1c5ced49ce1f7c09ea2591eda8991b7
jestupinanb <30636279+jestupinanb@users.noreply.github.com>
2020-12-13 15:29:52 -0500

    Create 522.java

---
+++ input/jaime/522.java
@@ -0,0 +1,15 @@
+
+public class Spaghetti {
+    public static void main(String[] args) {
+    }
+}
+
+public class spaghetti {
+    public static void main(String[] args) {
+    }
+}
+
+public class sp$aghetti {
+    public static void main(String[] args) {
+    }
+}

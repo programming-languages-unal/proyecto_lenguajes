48f7892897497f953dbfdb2b3121924436c954e0
juan <jpbmdev@gmail.com>
2020-12-12 23:04:31 -0500

    more bug fix

--- input/in.java
+++ input/in.java
@@ -6,6 +6,47 @@ class O {
 
     void Empty(){}
 
+    int Mars() {
+        final int LoleOne = 0;
+        int loleTwo = 0;
+
+        switch (LoleOne) {
+            case 1:
+            case 2:
+            case 3:
+            default:
+        }//
+        return LoleOne;
+    };
+
+    int Venus() { //no debe tener problema
+        //String args2 [];
+        int loleOne = 0;
+        if(true){
+
+        } else {
+
+        };
+        loleOne++;
+        if(true){
+
+        } else if(true) {
+
+        };
+        try {
+
+        } catch (Exception e){
+
+        }
+        return 0;
+    }
+}
+
+class TestThirdRule {
+    private final static int NUMBER_CONSTANT=5;//no debe tener problema
+    final static long df=8;//violacion de 5.2.4 las constantes deben tener solo mayusculas y undercore
+    static int ss=0;//no marca problema pues deber ser final y static para ser una constante
+
     int Mars() {  //no debe tener problema
 
         final int LoleOne = 0;
@@ -19,27 +60,4 @@ class O {
         }//
         return LoleOne;
     };
-
-    int Venus() {
-        //String args2 [];
-        int loleOne = 0;
-        if(true){
-
-        } else {
-
-        };
-        loleOne++;
-        try {
-
-        } catch (Exception e){
-
-        }
-        return 0;
-    }
-}
-
-class TestThirdRule {
-    private final static int NUMBER_CONSTANT=5;//no debe tener problema
-    final static long df=8;//violacion de 5.2.4 las constantes deben tener solo mayusculas y undercore
-    static int as=0;//no marca problema pues deber ser final y static para ser una constante
 }
